b790e356217095686973ff7d5a65acbdd426564e
Abby Adams <abby@1996thelabel.org>
2021-01-01 18:14:22 -0500

    reset html, added styling, and Timer

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-// Var with array and object for questions 
+// Var for questions 
 var questions = [
   {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "What did we cover in the first 2 weeks of class?",
+      choices: ["html & CSS ", "Web APIs", "computer science", "Advanced Javascript"],
+      answer: "html & CSS"
   },
   {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "What is Abby's favorite thing to learn about?",
+      choices: ["Javascript", "MySQL", "Computer Science", "Git"],
+      answer: "MySQL"
   },
   {
-      title: "Arrays in Javascript can be used to store ____.",
-      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      answer: "all of the above"
+      title: "What is git used for",
+      choices: ["Collaboritive work", "Github", "Version Control", "All of the above"],
+      answer: "All of the above"
   },
   {
-      title: "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-      answer: "quotes"
+      title: "What was Jonathon's advice for being successful in this class?",
+      choices: ["Always be coding", "Don't ask questions", "Reading is more imprtant than doing", "parenthesis"],
+      answer: "Always be coding"
   },
   {
-      title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-      choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-      answer: "console log"
+      title: "What is a hackathon?",
+      choices: ["A sandwich", "A programming language", "tool for beginners", "A competition"],
+      answer: "A competition"
   },
 
 ];
+
+
+
+
+// Variables
+var score = 0;
+var questionIndex = 0;
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+var timer = document.querySelector("#startTime");
+var currentTime = document.querySelector("#currentTime");
+
+
+
+var secondsLeft = 106;
+
+var holdInterval = 0;
+
+var penalty = 15;
+
+var ulCreate = document.createElement("ul");
+
+// Timer Display 
+timer.addEventListener("click", function () {
+    // set to zero
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
