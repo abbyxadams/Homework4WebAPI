460bbbdfd9123dceb5d897cd98d486668b8b1cbd
Abby Adams <abby@1996thelabel.org>
2021-01-06 15:32:21 -0600

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Homework4WebAPI
-AS A coding boot camp student I WANT to take a timed quiz on JavaScript fundamentals that stores high scores SO THAT I can gauge my progress compared to my peers ```
+AS A coding boot camp student I WANT to take a timed quiz on our Coding Bootcamp Course that stores high scores SO THAT I can gauge my progress compared to my peers ```
--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 var questions = [
   {
       title: "What did we cover in the first 2 weeks of class?",
-      choices: ["html & CSS ", "Web APIs", "computer science", "Advanced Javascript"],
+      multiChoice: ["html & CSS ", "Web APIs", "computer science", "Advanced Javascript"],
       answer: "html & CSS"
   },
   {
       title: "What is Abby's favorite thing to learn about?",
-      choices: ["Javascript", "MySQL", "Computer Science", "Git"],
+      multiChoice: ["Javascript", "MySQL", "Computer Science", "Git"],
       answer: "MySQL"
   },
   {
       title: "What is git used for",
-      choices: ["Collaboritive work", "Github", "Version Control", "All of the above"],
+      multiChoice: ["Collaboritive work", "Github", "Version Control", "All of the above"],
       answer: "All of the above"
   },
   {
       title: "What was Jonathon's advice for being successful in this class?",
-      choices: ["Always be coding", "Don't ask questions", "Reading is more imprtant than doing", "parenthesis"],
+      multiChoice: ["Always be coding", "Don't ask questions", "Reading is more imprtant than doing", "parenthesis"],
       answer: "Always be coding"
   },
   {
       title: "What is a hackathon?",
-      choices: ["A sandwich", "A programming language", "tool for beginners", "A competition"],
+      multiChoice: ["A sandwich", "A programming language", "tool for beginners", "A competition"],
       answer: "A competition"
   },
 
@@ -31,39 +31,188 @@ var questions = [
 
 
 
-// Variables
-var score = 0;
-var questionIndex = 0;
-var questionsDiv = document.querySelector("#questionsDiv");
-var wrapper = document.querySelector("#wrapper");
-var timer = document.querySelector("#startTime");
-var currentTime = document.querySelector("#currentTime");
+// Start Time
+let secondsLeft = 76;
+
+//Timer Display 
+let timer = document.getElementById("timer");
+
+//High Score
+let scoresDiv = document.getElementById("scores-div");
+
+let buttonsDiv = document.getElementById("buttons")
+
+let viewScoresBtn = document.getElementById("view-scores")
+
+//Start Button
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", setTime);
 
 
+var questionDiv = document.getElementById("question-div");
 
-var secondsLeft = 106;
+let results = document.getElementById("results");
 
-var holdInterval = 0;
+// Choices Div 
+var choices = document.getElementById("choices");
 
-var penalty = 15;
 
-var ulCreate = document.createElement("ul");
+// High Scores
+let emptyArray = [];
 
-// Timer Display 
-timer.addEventListener("click", function () {
-    // set to zero
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function () {
-            secondsLeft--;
-            currentTime.textContent = "Time: " + secondsLeft;
+// Local Storage
+let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
 
-            if (secondsLeft <= 0) {
-                clearInterval(holdInterval);
-                allDone();
-                currentTime.textContent = "Time's up!";
-            }
-        }, 1000);
+
+var questionCount = 0;
+
+
+let score = 0
+
+
+function setTime() {
+  displayQuestions();
+  let timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "";
+    timer.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <= 0 || questionCount === questions.length) {
+      clearInterval(timerInterval);
+      captureUserScore();
+    } 
+  }, 1000);
+}
+
+function displayQuestions() {
+  removeEls(startButton);
+
+  if (questionCount < questions.length) {
+    questionDiv.innerHTML = questions[questionCount].title;
+    choices.textContent = "";
+
+    for (let i = 0; i < questions[questionCount].multiChoice.length; i++) {
+      let el = document.createElement("button");
+      el.innerText = questions[questionCount].multiChoice[i];
+      el.setAttribute("data-id", i);
+      el.addEventListener("click", function (event) {
+        event.stopPropagation();
+
+        if (el.innerText === questions[questionCount].answer) {
+          score += secondsLeft;
+        } else {
+          score -= 10;
+          secondsLeft = secondsLeft - 15;
+        }
+        
+        questionDiv.innerHTML = "";
+
+        if (questionCount === questions.length) {
+          return;
+        } else {
+          questionCount++;
+          displayQuestions();
+        }
+      });
+      choices.append(el);
     }
-    render(questionIndex);
-});
+  }
+}
 
+
+function captureUserScore() {
+  timer.remove();
+  choices.textContent = "";
+
+  let initialsInput = document.createElement("input");
+  let postScoreBtn = document.createElement("input");
+
+  results.innerHTML = `Great Job! You scored ${score} points! Enter your initials here: `;
+  initialsInput.setAttribute("type", "text");
+  postScoreBtn.setAttribute("type", "button");
+  postScoreBtn.setAttribute("value", "Keep My Score!");
+  postScoreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+    let initials = initialsInput.value;
+    let userAndScore = {
+      initials: initials,
+      score: score,
+    };
+
+    scoresArray.push(userAndScore);
+    saveScores(scoresArray);
+    displayAllScores();
+    clearScoresBtn();
+    goBackBtn();
+    viewScoresBtn.remove();
+  });
+  results.append(initialsInput);
+  results.append(postScoreBtn);
+}
+
+const saveScores = (array) => {
+  window.localStorage.setItem("highScores", JSON.stringify(array));
+}
+
+const defineScoresArray = (arr1, arr2) => {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+}
+
+const removeEls = (...els) => {
+  for (let el of els) el.remove();
+}
+
+function displayAllScores() {
+  removeEls(timer, startButton, results);
+  let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+  scoresArray.forEach(obj => {
+    let initials = obj.initials;
+    let storedScore = obj.score;
+    let resultsP = document.createElement("p");
+    resultsP.innerText = `${initials}: ${storedScore}`;
+    scoresDiv.append(resultsP);
+  });
+}
+
+function viewScores() {
+  viewScoresBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    removeEls(timer, startButton);
+    displayAllScores();
+    removeEls(viewScoresBtn);
+    clearScoresBtn();
+    goBackBtn();
+  });
+}
+
+function clearScoresBtn() {    
+  let clearBtn = document.createElement("input");
+  clearBtn.setAttribute("type", "button");
+  clearBtn.setAttribute("value", "Clear Scores");
+  clearBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    removeEls(scoresDiv);
+    window.localStorage.removeItem("highScores");
+  })
+  scoresDiv.append(clearBtn)
+}
+
+function goBackBtn() {
+  let backBtn = document.createElement("input");
+  backBtn.setAttribute("type", "button");
+  backBtn.setAttribute("value", "Go Back");
+  backBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.reload();
+  })
+  buttonsDiv.append(backBtn)
+}
+
+
+viewScores();
